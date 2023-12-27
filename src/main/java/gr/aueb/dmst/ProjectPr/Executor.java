c3f71221522e122000c3ef46ff9e1157574d83c2
= <t8210063@aueb.gr>
2023-12-27 15:19:41 +0200

    Added new Executor and resources image files.

--- src/main/java/gr/aueb/dmst/ProjectPr/Executor.java
+++ src/main/java/gr/aueb/dmst/ProjectPr/Executor.java
@@ -1,8 +1,9 @@
 package gr.aueb.dmst.ProjectPr;
 
-import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.exception.BadRequestException;
+import com.github.dockerjava.api.exception.ConflictException;
 
 /** The Executor Thread Class. */
 public final class Executor {
@@ -19,101 +20,84 @@ public final class Executor {
      *
      * @param dockerClientMain The main dockerClient used to execute Docker commands.
      */
-    public static void setUpExecutor(final DockerClient dockerClientMain) {
+    public static void setUpExecutor(DockerClient dockerClientMain) {
         Executor.dockerClient = dockerClientMain;
-    }
-
-    /** Method for Starting a Container.
-     * If the Container is already running, only notifies the user that no action was taken.
-     *
-     * @param container The container to be started.
-     */
-    public static void startContainer(final Container container) {
-        if ("running".equalsIgnoreCase(container.getState())) {
-            System.out.printf("The container %s is already running. No action taken.\n", ContainerModel.getFormattedName(container));
-        } else {
-            System.out.printf("Starting container %s... \n", ContainerModel.getFormattedName(container));
-            dockerClient.startContainerCmd(container.getId()).exec();
-            //waiting until the container's state turns to running
-            InspectContainerResponse containerInspect;
-            String containerState = "exited";
-            while ("exited".equalsIgnoreCase(containerState)) {
-                containerInspect = dockerClient.inspectContainerCmd(container.getId()).exec();
-                containerState = containerInspect.getState().getStatus();
-            }
-            System.out.printf("%s is now running. \n", ContainerModel.getFormattedName(container));
-        }
     }
 
     /** Method for Starting a Container.
      *
      * @param containerId The ID of the container to be started.
-     * @return An integer: 0 if the Container was already running or 1 if the Container was successfully started.
+     * @return An integer: 0 if the Container was started successfully, 1 otherwise.
      */
-    public static int startContainer(final String containerId) {
+    public static int startContainer(String containerId) {
         InspectContainerResponse containerInspect = dockerClient.inspectContainerCmd(containerId).exec();
         String containerState = containerInspect.getState().getStatus();
         if ("running".equalsIgnoreCase(containerState)) {
-            //System.out.printf("The container %.12s is already running. No action taken.\n", containerId);
-            return 0;
+            return 1;
         } else {
-            //System.out.printf("Starting container %.12s... \n", containerId);
-            dockerClient.startContainerCmd(containerId).exec();
-            //waiting until the container's state turns to running
+            try {
+                dockerClient.startContainerCmd(containerId).exec();
+            } catch (Exception e) {
+                return 1;
+            }
+            
             containerState = "exited";
             while ("exited".equalsIgnoreCase(containerState)) {
                 containerInspect = dockerClient.inspectContainerCmd(containerId).exec();
                 containerState = containerInspect.getState().getStatus();
             }
-            //System.out.printf("%.12s is now running. \n", containerId);
-            return 1;
-        }
-    }
-
-    /** Method for Stopping a Container.
-     * If the Container is already exited, only notifies the user that no action was taken.
-     *
-     * @param container The container to be stopped.
-     */
-    public static void stopContainer(final Container container) {
-        if ("exited".equalsIgnoreCase(container.getState())) {
-            System.out.printf("The container %s is already stopped. No action taken.\n", ContainerModel.getFormattedName(container));
-        } else {
-            System.out.printf("Stopping container %s... \n", ContainerModel.getFormattedName(container));
-            dockerClient.stopContainerCmd(container.getId()).exec();
-            //waiting until the container's state turns to exited
-            InspectContainerResponse containerInspect;
-            String containerState = "running";
-            while ("running".equalsIgnoreCase(containerState)) {
-                containerInspect = dockerClient.inspectContainerCmd(container.getId()).exec();
-                containerState = containerInspect.getState().getStatus();
-            }
-            System.out.printf("%s is no longer running. \n", ContainerModel.getFormattedName(container));
+            return 0;
         }
     }
 
     /** Method for Stopping a Container.
      *
      * @param containerId The ID of the container to be stopped.
-     * @return An integer: 0 if the Container was already exited or 1 if the Container was successfully stopped.
+     * @return An integer: 0 if the Container was stopped successfully, 1 otherwise.
      */
-    public static int stopContainer(final String containerId) {
+    public static int stopContainer(String containerId) {
         InspectContainerResponse containerInspect = dockerClient.inspectContainerCmd(containerId).exec();
         String containerState = containerInspect.getState().getStatus();
         if ("exited".equalsIgnoreCase(containerState)) {
-            //System.out.printf("The container %.12s is already stopped. No action taken.\n", containerId);
-            return 0;
+            return 1;
         } else {
-            //System.out.printf("Stopping container %.12s... \n", containerId);
-            dockerClient.stopContainerCmd(containerId).exec();
-            //waiting until the container's state turns to exited
+            try {
+                dockerClient.stopContainerCmd(containerId).exec();
+            } catch (Exception e) {
+                return 1;
+            }
+            
             containerState = "running";
             while ("running".equalsIgnoreCase(containerState)) {
                 containerInspect = dockerClient.inspectContainerCmd(containerId).exec();
                 containerState = containerInspect.getState().getStatus();
             }
-            //System.out.printf("%.12s is no longer running. \n", containerId);
-            return 1;
+            return 0;
         }
     }
+
+    public static String createContainer(String imageName, String containerName) {
+        try {
+            dockerClient.createContainerCmd(imageName+":latest")
+                        .withName(containerName)
+                        .exec();
+            return "Created new container called " + containerName + " with Image " + imageName + ".";
+        } catch (BadRequestException e) {
+            return "Invalid container name. Only [a-zA-Z0-9][a-zA-Z0-9_.-] are allowed.";
+        } catch (ConflictException e) {
+            return "Invalid container name. The container name " + containerName + " is already in use.";
+        } catch (Exception e) {
+            return "Something went wrong when attempting to create a new container.";
+        }
+    }
+
+    public static String createContainer(String imageName) {
+        try {
+            dockerClient.createContainerCmd(imageName+":latest")
+                        .exec();
+            return "Created new container with Image " + imageName + ".";
+        } catch (Exception e) {
+            return "Something went wrong when attempting to create a new container.";
+        }
+    } 
 }
