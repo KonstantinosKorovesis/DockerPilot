63139147302f48ebf1b330063796be2f5bf4ae66
= <t8210063@aueb.gr>
2024-01-02 15:37:28 +0200

    Update API and HTTPRequest classes

--- src/main/java/gr/aueb/dmst/ProjectPr/API.java
+++ src/main/java/gr/aueb/dmst/ProjectPr/API.java
@@ -1,187 +1,76 @@
 package gr.aueb.dmst.ProjectPr;
 
+import java.util.List;
+import com.google.gson.Gson;
 import static spark.Spark.*;
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.nio.charset.StandardCharsets;
-
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Image;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient;
-import java.time.Duration;
 
 public class API {
-    public static void start() {
-        //http://localhost:8080
-        //staticFileLocation("/api_resources");
-        port(8080);
-        initExceptionHandler((e) -> System.err.println("[ERROR] Something went wrong when initializing the server."));
+    public static void start() {      
+        port(8080); //http://localhost:8080
+        initExceptionHandler((e) -> System.err.println("[ERROR API] Something went wrong when initializing the server."));
         init();
 
-        get("/info", (request, response) -> {
-            return "<html><body><h1>This is the info page of the application.</h1></body></html>";
-        });
-
-        get("/testing", (request, response) -> {
-            System.out.println("[REQUEST] " + request.toString());
-            System.out.println("[RESPONSE] " + response.toString());
-            return "<html><body><h1>Testing... Watch command-line.</h1></body></html>";
-        });
-
-        get("/container_states", (request, response) -> {
-            String date = request.queryParams("date");
-            //TODO: use date for queries, return running/stopped containers
-            return "param: " + date;
-        });
-
-        get("/stop_container", (request, response) -> {
+        post("/stop_container", (request, response) -> {
             String container_id = request.queryParams("container_id");
             int result = Executor.stopContainer(container_id);
             if (result == 0) {
-                return String.format("Container %12s was already exited. No action taken.", container_id);
+                return String.format("[API] Container %12s is now stopped. ", container_id);
             } else {
-                return String.format("Container %12s is now stopped. ", container_id);
+                return String.format("[API] Something went wrong when attempting to stop the container. No action taken.", container_id);
             }
         });
 
-        get("/start_container", (request, response) -> {
+        post("/start_container", (request, response) -> {
             String container_id = request.queryParams("container_id");
             int result = Executor.startContainer(container_id);
             if (result == 0) {
-                return String.format("Container %12s was already running. No action taken.", container_id);
+                return String.format("[API] Container %12s is now running. ", container_id);
             } else {
-                return String.format("Container %12s is now running. ", container_id);
+                return String.format("[API] Something went wrong when attempting to start the container. No action taken.", container_id);
             }
         });
 
-        post("/submit", (request, response) -> {
-            String body = request.body();
-
-            //use body in program
-            System.out.println("[Received POST data] " + body);
-            
-            response.status(200);
-            return body;
+        get("/running_container_count", (request, response) -> {
+            String measurementId = request.queryParams("measurementId");
+            int count = Database.selectRunningContainerCount(measurementId);
+            return String.valueOf(count);
         });
 
-        /*
-        try {
-            DockerClientConfig config = null;
-            DockerHttpClient httpClient = null;
-            DockerClient dockerClient = null;
-            //set up and configure the dockerClient used for executing Docker commands.
-            config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                                              .withDockerHost("tcp://localhost:2375")
-                                              .build();
-            httpClient = new ApacheDockerHttpClient.Builder()
-                                                   .dockerHost(config.getDockerHost())
-                                                   .sslConfig(config.getSSLConfig())
-                                                   .maxConnections(100)
-                                                   .connectionTimeout(Duration.ofSeconds(30))
-                                                   .responseTimeout(Duration.ofSeconds(45))
-                                                   .build();
-            dockerClient = DockerClientImpl.getInstance(config, httpClient);
+        get("/all_container_count", (request, response) -> {
+            String measurementId = request.queryParams("measurementId");
+            int count = Database.selectTotalContainerCount(measurementId);
+            return String.valueOf(count);
+        });
 
-            //set up Monitor and Executor with the dockerClient object
-            Monitor.setUpMonitor(dockerClient);
-            Executor.setUpExecutor(dockerClient);
+        get("/available_dates", (request, response) -> {
+            String[] dates = Database.selectAllDates();
+            Gson gson = new Gson();
+            String jsonDates = gson.toJson(dates);
+            response.type("application/json");
+            return jsonDates;
+        });
 
+        get("/available_measurements", (request, response) -> {
+            String date = request.queryParams("date");
+            String[] measurementIds = Database.selectMeasurementIdsOnDate(date);
+            Gson gson = new Gson();
+            String jsonIds = gson.toJson(measurementIds);
+            response.type("application/json");
+            return jsonIds;
+        });
 
-
-            stop_container("8d171c6c0ba6");
-            //System.out.println("[Server will close in 60 seconds...]");
-            Thread.sleep(6000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("[Server is closing...]");
-            stop();
-            System.out.println("[Server closed.]");
-        }
-         */
+        get("/container_entries", (request, response) -> {
+            String measurementId = request.queryParams("measurementId");
+            List<String[]> containerEntries = Database.selectContainerEntries(measurementId);
+            Gson gson = new Gson();
+            String jsonEntries = gson.toJson(containerEntries);
+            response.type("application/json");
+            return jsonEntries;
+        });
     }
 
     public static void stop() {
         System.out.println("Server is closing...");
         spark.Spark.stop();
-    }
-    
-    public static void post_testing() {
-        try {
-            String urlString = "http://localhost:8080/submit";
-
-            String postData = "key1=value1&key2=value2";
-
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setDoOutput(true);
-
-            //write POST data to the connection's output stream
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] postDataBytes = postData.getBytes(StandardCharsets.UTF_8);
-                os.write(postDataBytes);
-                os.flush();
-            }
-
-            //get response code (should be 200)
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            //get the response body
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String line;
-                StringBuilder response = new StringBuilder();
-                while ((line = in.readLine()) != null) {
-                    response.append(line);
-                }
-                System.out.println("POST Response: " + response.toString());
-            }
-        } catch (Exception e) {
-            System.err.println("[ERROR] Something went wrong when using the POST method.");
-            System.err.println(e);
-        }
-    }
-
-    public static void GET_container_states(String dateRequest) {
-        String baseUrl = "http://localhost:8080";
-        try {
-            //FOR STOPPING/ STARTING
-            //String endpoint = "/stop_container?container_id=" + container_id;
-            String endpoint = "/container_states?date=" + dateRequest;
-
-            URL url = new URL(baseUrl + endpoint);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            //get response code (should be 200)
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            //get the response
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
-                String line;
-                StringBuilder response = new StringBuilder();
-                while ((line = in.readLine()) != null) {
-                    response.append(line);
-                }
-                System.out.println("GET Response: " + response.toString());
-            }
-
-            connection.disconnect();
-        } catch (Exception e) {
-            System.err.println("[ERROR] Something went wrong when using the GET method.");
-            System.err.println(e);
-        }
     }
 }
--- src/main/java/gr/aueb/dmst/ProjectPr/HTTPRequest.java
+++ src/main/java/gr/aueb/dmst/ProjectPr/HTTPRequest.java
@@ -4,65 +4,225 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
 
 public class HTTPRequest {
+    private static final String baseUrl = "http://localhost:8080";
     public static void stop_container(String container_id) {
-        String baseUrl = "http://localhost:8080";
         try {
             String endpoint = "/stop_container?container_id=" + container_id;
 
             URL url = new URL(baseUrl + endpoint);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod("POST");
 
-            //get response code (should be 200)
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
 
-            //get the response
             try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
                 String line;
                 StringBuilder response = new StringBuilder();
                 while ((line = in.readLine()) != null) {
                     response.append(line);
                 }
-                System.out.println("Response: " + response.toString());
+                System.out.println(response);
             }
 
             connection.disconnect();
         } catch (Exception e) {
-            System.err.println("[ERROR] Something went wrong when using the stop_container method.");
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the stop_container method.");
             System.err.println(e);
         }
     }
 
     public static void start_container(String container_id) {
-        String baseUrl = "http://localhost:8080";
         try {
             String endpoint = "/start_container?container_id=" + container_id;
 
             URL url = new URL(baseUrl + endpoint);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod("POST");
 
-            //get response code (should be 200)
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
 
-            //get the response
             try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
                 String line;
                 StringBuilder response = new StringBuilder();
                 while ((line = in.readLine()) != null) {
                     response.append(line);
                 }
-                System.out.println("Response: " + response.toString());
+                System.out.println(response);
             }
 
             connection.disconnect();
         } catch (Exception e) {
-            System.err.println("[ERROR] Something went wrong when using the start_container method.");
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the start_container method.");
             System.err.println(e);
         }
+    }
+
+    public static int running_container_count(String measurementId) {
+        try {
+            String endpoint = "/running_container_count?measurementId=" + measurementId;
+
+            URL url = new URL(baseUrl + endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
+
+            int count;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+                count = Integer.valueOf(response.toString());
+            }
+            connection.disconnect();
+            return count;
+        } catch (Exception e) {
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the running_container_count method.");
+            System.err.println(e);
+        }
+        return 0;
+    }
+
+    public static int all_container_count(String measurementId) {
+        try {
+            String endpoint = "/all_container_count?measurementId=" + measurementId;
+
+            URL url = new URL(baseUrl + endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
+
+            int count;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+                count = Integer.valueOf(response.toString());
+            }
+            connection.disconnect();
+            return count;
+        } catch (Exception e) {
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the all_container_count method.");
+            System.err.println(e);
+        }
+        return 0;
+    }
+
+    public static String[] available_dates() {
+        try {
+            String endpoint = "/available_dates";
+
+            URL url = new URL(baseUrl + endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
+
+            String[] dates;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+
+                Gson gson = new Gson();
+                dates = gson.fromJson(response.toString(), String[].class);
+            }
+            connection.disconnect();
+            return dates;
+        } catch (Exception e) {
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the available_dates method.");
+            System.err.println(e);
+        }
+        return new String[0];
+    }
+
+    public static String[] available_measurements(String date) {
+        try {
+            String endpoint = "/available_measurements?date=" + date;
+
+            URL url = new URL(baseUrl + endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
+
+            String[] measurementIds;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+
+                Gson gson = new Gson();
+                measurementIds = gson.fromJson(response.toString(), String[].class);
+            }
+            connection.disconnect();
+            return measurementIds;
+        } catch (Exception e) {
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the available_measurements method.");
+            System.err.println(e);
+        }
+        return new String[0];
+    }
+
+    public static List<String[]> container_entries(String measurementId) {
+        List<String[]> containerEntries = new ArrayList<String[]>();
+        try {
+            String endpoint = "/container_entries?measurementId=" + measurementId;
+
+            URL url = new URL(baseUrl + endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (connection.getResponseCode() != 200) {
+                System.out.println("[HTTPRequest ERROR] Response Code: " + connection.getResponseCode());
+            }
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+
+                Gson gson = new Gson();
+                Type listType = new TypeToken<List<String[]>>() {}.getType();
+                containerEntries = gson.fromJson(response.toString(), listType);
+            }
+            connection.disconnect();
+            return containerEntries;
+        } catch (Exception e) {
+            System.err.println("[HTTPRequest ERROR] Something went wrong when using the container_entries method.");
+            System.err.println(e);
+        }
+        return containerEntries;
     }
 }
